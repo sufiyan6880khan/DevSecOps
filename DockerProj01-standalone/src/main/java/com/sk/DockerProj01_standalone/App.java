2e6cdf10ffb087efdbdc9d3c04b25c0a4fc5bdd8
sufiyan <[sufiyan6880khan@gmail.com]>
2024-11-29 20:39:40 +0530

    docker standalone tast --sufiyan

---
+++ DockerProj01-standalone/src/main/java/com/sk/DockerProj01_standalone/App.java
@@ -0,0 +1,14 @@
+package com.sk.DockerProj01_standalone;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+       int a =10;
+       int b= 10;
+    	System.out.println("Hello World!");
+        
+        System.out.println("Sum of a and b ::"+(a+b));
+    }
+}
